084a8debf3a44eeeb1fc38da30440e8f128569e9
crt-enter-cse <prasnnacharya@gmail.com>
2024-02-15 20:52:46 +0530

    2nd

--- src/knowBasicMaths/CheckForPrime.java
+++ src/knowBasicMaths/CheckForPrime.java
@@ -1,18 +1,33 @@
 package knowBasicMaths;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class CheckForPrime {
 
 	public static void main(String[] args) {
-		int number=3 ;
-		boolean flag= checkforprime(number);
-		
-		System.out.println(flag ==true ? "prime ":"non prime");
+
+		List arr = new ArrayList();
+		arr.add(23);
+
+		// to check the modifinable method which will not allow to change the list
+		Collections.unmodifiableList(arr);
+
+		for (Object i : arr) {
+			System.out.println(i);
+		}
+
+		int number = 3;
+		boolean flag = checkforprime(number);
+
+		System.out.println(flag == true ? "prime " : "non prime");
 	}
 
 	private static boolean checkforprime(int number) {
 
-		for( int i=2; i<Math.sqrt(number);i++) {
-			if(number%i==0) {
+		for (int i = 2; i < Math.sqrt(number); i++) {
+			if (number % i == 0) {
 				return false;
 			}
 		}
