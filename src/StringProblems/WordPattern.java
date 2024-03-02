e35aa79e25c97046075576e3ca7b1f90bf81e509
crt-enter-cse <prasnnacharya@gmail.com>
2024-03-02 19:02:01 +0530

    branch practice

--- src/StringProblems/WordPattern.java
+++ src/StringProblems/WordPattern.java
@@ -30,11 +30,15 @@ public class WordPattern {
 	    		
 	    	
 	    	String pattern = "abba", s = "dog cat cat dog";
+	    	
 	    	boolean flag =wordPattern(pattern,s);
+	    	
 	    	System.out.println(flag);
 	    	
 	    	pattern = "aaaa"; s = "dog cat cat dog";
+	    	
 	    	flag =wordPattern(pattern,s);
+	    	
 	    	System.out.println(flag);
 	    	
 		}
